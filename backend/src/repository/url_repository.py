b5af61b5b8d83355ab29f3cd2a380330fa3ef711
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-01-22 20:06:38 -0700

    Add get by url code & get by original url methods

--- backend/src/repository/url_repository.py
+++ backend/src/repository/url_repository.py
@@ -9,3 +9,9 @@ class UrlRepository(ABC):
     @abstractmethod
     def get_shortened_url_by_uuid(self, id_: int):
         pass
+
+    def get_shortened_url_by_url_code(self, url_code):
+        pass
+
+    def get_shortened_url_by_original_url(self, original_url):
+        pass
