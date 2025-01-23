2873dc2474337386073c7ef17c9de9fa4f79b1a6
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-01-22 17:00:53 -0700

    Only create table if not exists

--- backend/src/db/schema.py
+++ backend/src/db/schema.py
@@ -19,7 +19,7 @@ class SchemaCreator:
 
     def _create_shortened_urls_table(self):
         self.connection.execute("""
-            CREATE TABLE "urls" (
+            CREATE TABLE IF NOT EXISTS "urls" (
                 "uuid"	TEXT NOT NULL UNIQUE,
                 "original_url"	TEXT NOT NULL UNIQUE,
                 "url_code"	TEXT NOT NULL UNIQUE,
