b74c3d881d9b743756c9a5cc0115a0f4bf133f12
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-01-23 19:48:18 -0700

    Rename methods to reduce redundancy and improve clarity

--- backend/src/repository/url_repository.py
+++ backend/src/repository/url_repository.py
@@ -5,22 +5,22 @@ from backend.src.models import ShortenedUrl
 
 class UrlRepository(ABC):
     @abstractmethod
-    def save_shortened_url(self, url, shortened_url_code) -> ShortenedUrl:
+    def save(self, url, shortened_url_code) -> ShortenedUrl:
         pass
 
     @abstractmethod
-    def get_shortened_url_by_uuid(self, uuid_: str) -> ShortenedUrl:
+    def get_by_uuid(self, uuid_: str) -> ShortenedUrl:
         pass
 
     @abstractmethod
-    def get_shortened_url_by_url_code(self, url_code: str) -> ShortenedUrl:
+    def get_by_url_code(self, url_code: str) -> ShortenedUrl:
         pass
 
     @abstractmethod
-    def get_shortened_url_by_original_url(self, original_url: str) -> ShortenedUrl:
+    def get_by_original_url(self, original_url: str) -> ShortenedUrl:
         pass
 
     @abstractmethod
-    def get_all_shortened_urls(self) -> list[ShortenedUrl]:
+    def get_all(self) -> list[ShortenedUrl]:
         pass
 
