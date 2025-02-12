59d0e4cb0c0d21260003e96d09568f0f0ec69b4e
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-11 20:06:50 -0700

    Add url service creation utility

--- backend/app/services/url/__init__.py
+++ backend/app/services/url/__init__.py
@@ -1,1 +1,10 @@
 from .url_service import UrlService
+
+from backend.app.repository import get_url_repository
+from backend.app.services.code_generator import code_generator
+
+
+def get_url_service() -> UrlService:
+    get_code_length = lambda: 5 # TODO: Replace with actual implementation
+    url_repository = get_url_repository()
+    return UrlService(url_repository, code_generator, get_code_length)
