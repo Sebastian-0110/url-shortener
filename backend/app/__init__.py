d7ef02625bcb90bcb1d8b57be7c3acb4fbe90087
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-11 20:00:54 -0700

    Ensure db closing

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
 from .url_shortener import url_shortener
-
+from .db import close_db
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(url_shortener, url_prefix="/urls")
+
+    @app.teardown_appcontext
+    def ensure_db_closing(exception):
+        close_db()
 
     return app
