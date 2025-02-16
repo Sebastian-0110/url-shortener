54a4511be69a0210e301d4b63988f41c666bb502
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-15 21:47:48 -0700

    Add redirect view

---
+++ frontend/src/components/RedirectView.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import { watch } from "vue";
+    import { useRoute } from "vue-router";
+    import endpoint from "@/api/endpoint.js";
+
+    const route = useRoute();
+
+    watch(() => route.params.urlCode, async () => {
+        const response = await fetch(
+            endpoint(`/urls/${route.params.urlCode}/details`),
+            {
+                method: "GET",
+            },
+        );
+
+        if (response.ok) {
+            const data = await response.json();
+            window.location.href = data["original_url"];
+        }
+
+    }, { immediate: true });
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
