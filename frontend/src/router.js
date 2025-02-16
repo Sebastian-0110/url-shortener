1a8f50b2f5672748aa4ced198104b3629e9fda2d
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-15 21:46:14 -0700

    Add redirect route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import HomeView from "./components/HomeView.vue";
 import UrlDetailsView from "./components/UrlDetailsView.vue";
+import RedirectView from "@/components/RedirectView.vue";
 
 const routes = [
 	{ name: "home", path: "/", component: HomeView },
 	{ name: "details", path: "/details/:urlCode", component: UrlDetailsView },
+	{ name: "redirect", path: "/:urlCode", component: RedirectView },
 ];
 
 const router = createRouter({
