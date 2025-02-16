751e5919d3df73b1c7b5ffdf2e60aea53e802fc7
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-15 21:47:27 -0700

    Add details view

--- frontend/src/components/UrlDetailsView.vue
+++ frontend/src/components/UrlDetailsView.vue
@@ -1,11 +1,38 @@
 <script setup>
-</script>
+    import {ref, watch, computed} from "vue";
+    import { useRoute } from "vue-router";
+    import endpoint from "@/api/endpoint.js";
 
+    const route = useRoute();
+
+    const originalUrl = ref("");
+    const urlCode = ref("");
+
+    const shortenedUrl = computed(() => `${location.host}/${urlCode.value}`);
+
+    watch(() => route.params.uuid, async () => {
+        const response = await fetch(
+            endpoint(`/urls/${route.params.urlCode}/details`),
+            { method: "GET" })
+        ;
+
+        if (response.ok) {
+            const data = await response.json();
+
+            originalUrl.value = data["original_url"];
+            urlCode.value = data["url_code"];
+        }
+    }, { immediate: true });
+
+
+
+</script>
 
 <template>
 	<div class="container">
-		<p>Your url: {{ $route.params.url_code }}</p>
-		
+        <p>Original url: {{ originalUrl }}</p>
+        <p>Shortened url: {{ shortenedUrl }}</p>
+
         <RouterLink to="/">
             <button class="btn btn-primary">Go back</button>
         </RouterLink>
