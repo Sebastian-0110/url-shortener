c18c7253858b432f5954ae04c64269b47b99c8dc
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-16 01:25:36 -0700

    Change app name to HOCUS

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,9 +4,15 @@
 
 <template>
   <header class="bg-body-tertiary p-3">
-      <h1 class="m-0 text-center fw-bolder">Url Shortener</h1>
+      <h1 class="m-0 text-center fw-bolder">
+          <RouterLink :to="{ name: 'home' }">HOCUS</RouterLink>
+      </h1>
   </header>
 </template>
 
 <style scoped>
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
 </style>
