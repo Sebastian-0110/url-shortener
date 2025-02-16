5f2cd80c5cd2854769604b1c8b8d0b444f2e2ec6
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-16 02:08:56 -0700

    Open github page in another tab

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -6,7 +6,7 @@
     <footer>
         <p class="bg-body-tertiary m-0 p-3">
             Made with <span class="text-danger fw-bolder"><3</span> by
-            <a href="https://github.com/Sebastian-0110/">Sebastian Mendoza</a>
+            <a href="https://github.com/Sebastian-0110/" target="_blank">Sebastian Mendoza</a>
         </p>
     </footer>
 </template>
