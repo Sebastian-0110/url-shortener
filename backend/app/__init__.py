2f5aac1f560fdb03d719f82a4dcd553d00b28e71
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-17 02:32:16 -0700

    Delegate error hanlding to flask's error handlers

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 
+from .exceptions.domain_exception import DomainException
 from .url_shortener import url_shortener
 from .db import close_db
 
@@ -7,6 +8,10 @@ def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(url_shortener, url_prefix="/urls")
+
+    @app.errorhandler(DomainException)
+    def handle_exception(e):
+        return jsonify({ "error": e.message }), e.status_code
 
     @app.teardown_appcontext
     def ensure_db_closing(exception):
--- backend/app/url_shortener/routes.py
+++ backend/app/url_shortener/routes.py
@@ -1,35 +1,27 @@
-from flask import request, jsonify, redirect
+from flask import request, jsonify
 
 from . import url_shortener
 from backend.app.services.url import get_url_service
 from backend.app.repository import get_url_repository
-from backend.app.exceptions import ShortenedUrlNotFound, InvalidUrlException
 
 
 @url_shortener.post("/")
 def create():
     url = request.json.get("url", None)
 
-    try:
-        service = get_url_service()
-        shortened_url = service.save(url)
+    service = get_url_service()
+    shortened_url = service.save(url)
 
-        response = jsonify(shortened_url.model_dump(mode="json"))
-        response.status_code = 201
-        response.headers["location"] = f"/{shortened_url.url_code}"
+    response = jsonify(shortened_url.model_dump(mode="json"))
+    response.status_code = 201
+    response.headers["location"] = f"/{shortened_url.url_code}"
 
-        return response
-
-    except InvalidUrlException as e:
-        return jsonify({"error": e.message}), e.status_code
+    return response
 
 
 @url_shortener.get("/<string:url_code>/details")
 def details(url_code):
-    try:
-        repository = get_url_repository()
-        shortened_url = repository.get_by_url_code(url_code)
-        return jsonify(shortened_url.model_dump(mode="json")), 200
+    repository = get_url_repository()
+    shortened_url = repository.get_by_url_code(url_code)
 
-    except ShortenedUrlNotFound as e:
-        return jsonify({ "error": e.message }), e.status_code
+    return jsonify(shortened_url.model_dump(mode="json")), 200
