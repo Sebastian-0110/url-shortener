ec962cc67d15b310cf64a3964a95c21a342d09ef
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-02-16 21:35:10 -0700

    Improve ux by adding placeholders while fetching

--- frontend/src/components/UrlDetailsView.vue
+++ frontend/src/components/UrlDetailsView.vue
@@ -1,28 +1,34 @@
 <script setup>
     import { ref, watch, computed } from "vue";
-    import { useRoute } from "vue-router";
+    import { useRouter, useRoute } from "vue-router";
     import { push } from "notivue";
     import endpoint from "@/api/endpoint.js";
 
+    const router = useRouter();
     const route = useRoute();
 
     const originalUrl = ref("");
     const urlCode = ref("");
+    const isFetching = ref(false);
 
     const shortenedUrl = computed(() => `${location.origin}/${urlCode.value}`);
 
     watch(() => route.params.uuid, async () => {
+        isFetching.value = true;
         const response = await fetch(
             endpoint(`/urls/${route.params.urlCode}/details`),
-            { method: "GET" })
-        ;
+            { method: "GET" }
+        );
+        const data = await response.json();
 
-        if (response.ok) {
-            const data = await response.json();
-
-            originalUrl.value = data["original_url"];
-            urlCode.value = data["url_code"];
+        if (!response.ok) {
+            push.error(data.error);
+            return await router.push({ name: "home" });
         }
+
+        originalUrl.value = data["original_url"];
+        urlCode.value = data["url_code"];
+        isFetching.value = false;
     }, { immediate: true });
 
     async function copyToClipboard() {
@@ -40,16 +46,27 @@
 
         <div class="row justify-content-center gy-2">
             <div class="col col-12 col-sm-6">
-                <input class="form-control form-control-lg" :value="shortenedUrl" readonly />
+                <span class="form-control form-control-lg text-start">
+                    <span v-if="isFetching" class="placeholder placeholder-wave col-12 rounded"></span>
+                    <span v-else>{{ shortenedUrl }}</span>
+                </span>
             </div>
 
             <div class="col col-12 col-sm-auto">
-                <button class="btn btn-primary btn-lg w-100" @click="copyToClipboard">Copy</button>
+                <button
+                    class="btn btn-primary btn-lg w-100"
+                    @click="copyToClipboard"
+                    :disabled="isFetching"
+                >Copy</button>
             </div>
         </div>
 
         <div>
-            <p class="form-text mt-2 mb-5">This url takes you to: {{ originalUrl }}</p>
+            <p class="form-text mt-2 mb-5">
+                This url takes you to:
+                <span v-if="isFetching" class="placeholder placeholder-wave col-3 rounded"></span>
+                <span v-else>{{ originalUrl }}</span>
+            </p>
         </div>
 
         <RouterLink to="/">
